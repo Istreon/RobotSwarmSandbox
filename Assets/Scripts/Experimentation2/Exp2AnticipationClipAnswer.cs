39cd43bde28979c0fbbd9db7c2b370694ea2ede1
Istreon <47358771+Istreon@users.noreply.github.com>
2023-09-27 16:35:09 +0200

    Creation of the 2nd task player
    - (New) A script that will manage the second task of the experimentation

--- Assets/Scripts/Experimentation2/Exp2AnticipationClipAnswer.cs
+++ Assets/Scripts/Experimentation2/Exp2AnticipationClipAnswer.cs
@@ -3,10 +3,12 @@ public class Exp2AnticipationAnswer
 {
     public string filename;
     public bool fracture;
+    public float height;
 
-    public Exp2AnticipationAnswer(string filename, bool fracture)
+    public Exp2AnticipationAnswer(string filename, bool fracture, float height)
     {
         this.filename = filename;
         this.fracture = fracture;
+        this.height = height;
     }
 }
